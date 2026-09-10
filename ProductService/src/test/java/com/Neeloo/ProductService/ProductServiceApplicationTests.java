dcd4715adda6045cfd8a444edbb781cc8b3828c3
neeloo patel <nilusingh945164@gmai.com>
2026-09-10 17:50:54 +0530

    Backend project

---
+++ ProductService/src/test/java/com/Neeloo/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Neeloo.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
