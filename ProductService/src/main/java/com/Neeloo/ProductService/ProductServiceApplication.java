c03efaeaf0e6127d4994a295387cdb37652d77e5
neeloo patel <nilusingh945164@gmail.com>
2026-09-12 18:03:30 +0530

    backend

--- ProductService/src/main/java/com/Neeloo/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/Neeloo/ProductService/ProductServiceApplication.java
@@ -9,7 +9,7 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 
         SpringApplication.run(ProductServiceApplication.class, args);
-        System.out.println("Hello");
+        //System.out.println("Hello");
 	}
 
 }
