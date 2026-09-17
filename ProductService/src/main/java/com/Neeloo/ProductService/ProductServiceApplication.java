32a29137d49e3a1bc3d4e8fbb0059c91ed282771
neeloo patel <nilusingh945164@gmail.com>
2026-09-17 17:25:40 +0530

    backend

--- ProductService/src/main/java/com/Neeloo/ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com/Neeloo/ProductService/ProductServiceApplication.java
@@ -2,7 +2,6 @@ package com.Neeloo.ProductService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ProductServiceApplication {
 	public static void main(String[] args) {
